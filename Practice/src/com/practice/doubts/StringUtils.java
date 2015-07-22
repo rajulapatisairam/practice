904b2df47f2d8b52239e7457995f71773f60520e
rajulapatisairam <rajulapatisairam@gmail.com>
2015-07-22 23:35:20 +0530

    String prepared satement.....

--- Practice/src/com/practice/doubts/StringUtils.java
+++ Practice/src/com/practice/doubts/StringUtils.java
@@ -1,10 +1,26 @@
 package com.practice.doubts;
 
-
 public class StringUtils {
 public static void main(String...str) {
-	String input="/Practice/errro/error.jsp";
-	org.apache.commons.lang3.StringUtils.substring(input, 2);
-	System.out.println("\n Input is: "+input);
+	String checkLists[]={"1:123","2:345","3:785"};
+	updatQuery("A", 789, checkLists);
+}
+private static void updatQuery(String status,int userId,String...chekLists){
+	String query=" Update xyz set status ="+status+", userId ="+userId+" where id in(%s) and billNo in(%s) ";
+	String ids="";
+	String billNumbers="";
+	String coma=",";
+	
+	for(String checkList : chekLists)
+	{
+		String[] data = checkList.split(":");
+		ids = ids+data[0]+coma;
+		billNumbers = billNumbers+data[1]+coma;
+	}
+	ids = org.apache.commons.lang3.StringUtils.removeEndIgnoreCase(ids, coma);
+	billNumbers = org.apache.commons.lang3.StringUtils.removeEndIgnoreCase(billNumbers, coma);
+	query = String.format(query, ids,billNumbers);
+	System.out.println(query);
+	
 }
 }
