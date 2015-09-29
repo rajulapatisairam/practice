d745b11bb49e071e25eb9dcbda4539d7337cf36c
Sairam Rajulapati <rajulapatisairam@gmail.com>
2015-09-29 20:21:24 +0530

    Create MapUtils.java

---
+++ Practice/src/com/practice/utils/MapUtils.java
@@ -0,0 +1,47 @@
+package com.sutisoft.roe.util.collections;
+
+import java.util.Map;
+import org.apache.commons.lang.xwork.StringUtils;
+
+/**
+ *  <h1> MapUtils</h1>
+ *  MapUtils  is the generic class and it's 
+ *  allow the application to perform map related util funtionalities.
+ * 
+ * 
+ * @author Sairam Rajulapati
+ * @version 1.1.1 13, May 2015 
+ */
+public class MapUtils<KEY,VALUE> {
+	private KEY key;
+	private VALUE value;
+	public MapUtils(){
+	}
+	private MapUtils(KEY key,VALUE value){
+		this.key=key;
+		this.value=value;
+	}
+private final int KEY=1;
+private final int VALUE=1;
+private String getVALUEalues(Map<KEY,VALUE> map,String separator,int type)
+{
+	String output="";
+	for (Map.Entry<KEY,VALUE> entry : map.entrySet())
+	{
+		output+=type==VALUE?entry.getValue():entry.getKey();
+		output+=separator;
+	}
+	output=StringUtils.removeEndIgnoreCase(output, separator);
+	return output;
+}
+
+public String getValuesWithSeparator(Map<KEY,VALUE> map,String separator)
+{
+	return getVALUEalues(map, separator, VALUE);
+}
+
+public String getKeysWithSeparator(Map<KEY,VALUE> map,String separator)
+{
+	return getVALUEalues(map, separator, KEY);
+}
+}
