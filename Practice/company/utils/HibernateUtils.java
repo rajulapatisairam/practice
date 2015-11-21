ac8e3ee4799370709bcc7ccb1a433fb82c2a5a46
Sairam Rajulapati <rajulapatisairam@gmail.com>
2015-11-21 14:41:50 +0530

    Create HibernateUtils.java

---
+++ Practice/company/utils/HibernateUtils.java
@@ -0,0 +1,43 @@
+package com.sutisoft.roe.util.hibernate;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import com.sutisoft.roe.hibernate.HibernateSessionFactory;
+import com.sutisoft.roe.util.LoggerUtils;
+
+public class HibernateUtils<Entity> {
+	public static final Logger LOGGER = Logger.getLogger(HibernateUtils.class);
+        private Entity entity;
+        private Session session;
+    	private Criteria criteria;
+
+			private HibernateUtils() {
+				super();
+			}
+			public HibernateUtils(Entity entity) {
+				if(entity == null) throw new NullPointerException();
+				session = HibernateSessionFactory.getSession();
+				criteria = session.createCriteria(entity.getClass());
+				this.entity = entity;
+			}
+ 
+			@SuppressWarnings("unchecked")
+			public List<Entity> getEntityData(Map<String,Object> criterias){
+				LOGGER.info(LoggerUtils.methodStartingMessage());
+				for (Map.Entry<String, Object> condition : criterias.entrySet()) {
+					criteria.add(Restrictions.eq(condition.getKey(), condition.getValue()));
+			}
+				List<Entity> result = criteria.list(); 
+				session.clear();
+				session.close();
+				LOGGER.info(LoggerUtils.methodEndingMessage());
+				return result;
+			}
+        
+}
